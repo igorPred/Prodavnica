88bef00a3a04ad33f7979f62a39c8ea4f8e4f6a6
bilja <bbiljic@yahoo.com>
2019-04-10 21:50:03 +0200

    cfg i amdmin strana

---
+++ Prodavnica/src/model/Admin.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Admin {
+
+	
+	
+	
+	final String AdminUserName ="admin";
+	final String AdminPassword="admin";
+	
+	
+	
+	public String getAdminUserName() {
+		return AdminUserName;
+	}
+	public String getAdminPassword() {
+		return AdminPassword;
+	}
+	
+	
+	
+}
